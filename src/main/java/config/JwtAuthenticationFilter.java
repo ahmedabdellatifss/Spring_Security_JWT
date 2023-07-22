83b73c50026d46564382d042558714f59c9f2fed
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2023-07-23 01:45:25 +0300

    Generate JWT token

--- src/main/java/config/JwtAuthenticationFilter.java
+++ src/main/java/config/JwtAuthenticationFilter.java
@@ -15,15 +15,28 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
+	
+	private final JwtService jwtService;
 
 	@Override
 	protected void doFilterInternal(
 			@NonNull HttpServletRequest request, 
 			@NonNull HttpServletResponse response,
 			@NonNull FilterChain filterChain
-			)
-			throws ServletException, IOException {
+			
+		)throws ServletException, IOException {
+			
+		final String authHeader = request.getHeader("Authorization");
+		final String jwt;
+		final String userEmail;
 		
+		if( authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return ;
+		}
+		
+		jwt = authHeader.substring(7);
+		userEmail = jwtService.extractUsername(jwt);// todo extract the userEmail from the JWT token;
 		
 	}
 
--- src/main/java/config/JwtService.java
+++ src/main/java/config/JwtService.java
@@ -1,12 +1,18 @@
 package config;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import java.util.Date;
+import java.util.HashMap;
+
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -22,12 +28,35 @@ public class JwtService {
 	
 	
 	
+	
+	
 	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
 		
 		final Claims claims = extractAllClaims(token);
 		return claimsResolver.apply(claims);
 		
 	}
+	
+	
+	public String generateToken(UserDetails userDetails) {
+		
+		return generateToken(new HashMap<>(), userDetails);
+	}
+	
+	
+	public String generateToken( Map<String, Object> extraClaims, UserDetails userDetails ) {
+			
+			return Jwts
+					.builder()
+					.setClaims(extraClaims)
+					.setSubject(userDetails.getUsername())
+					.setIssuedAt(new Date(System.currentTimeMillis()))
+					.setExpiration(new Date(System.currentTimeMillis() + 1000*60*24))
+					.signWith(getSignInKey(), SignatureAlgorithm.HS256)
+					.compact();
+			
+		}
+	
 	
 	private Claims extractAllClaims(String token) {
 		
@@ -39,6 +68,10 @@ public class JwtService {
 				.getBody();
 	}
 
+	
+	
+	
+	
 	private Key getSignInKey() {
 		
 		byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
