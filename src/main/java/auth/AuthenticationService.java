60d662370e7170e7fa0b3b27dbda67d3b32753bb
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2023-07-25 01:09:05 +0300

    Create Authenicatio Package

---
+++ src/main/java/auth/AuthenticationService.java
@@ -0,0 +1,53 @@
+package auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.roaa.security.user.Role;
+import com.roaa.security.user.UserRepository;
+
+import config.JwtService;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+	
+	private final UserRepository repository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
+	private final AuthenticationManager authenticationManager;
+	
+	
+	
+	public AuthenticationResponse register(RegisterRequest request) {
+		var user = User.builder().
+				.firstname(request.getFirstname())
+				.lastname(request.getLastname())
+				.email(request.getEmail())
+				.password(passwordEncoder.encode(request.getPassword()))
+				.role(Role.USER)
+				.build();
+		repository.save(user);
+		var jwtToken = jwtService.generateToken(user);
+		return AuthenticationResponse.builder()
+				.token(jwtToken)
+				.build();
+	}
+
+	public AuthenticationResponse authenticate(AuthenticationRequest request) {
+		
+		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+		
+		var user = repository.findByEmail(request.getEmail())
+				.orElseThrow();
+		var jwtToken = jwtService.generateToken(user);
+		return AuthenticationResponse.builder()
+				.token(jwtToken)
+				.build();
+	}
+
+}
