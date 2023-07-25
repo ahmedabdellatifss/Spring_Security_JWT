320a6839157521f6c35a74786a2f5c349255e53f
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2023-07-25 22:54:28 +0300

    Create Demo Controller

--- src/main/java/config/SecurityConfiguration.java
+++ src/main/java/config/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration {
 			.csrf()
 			.disable()
 			.authorizeHttpRequests()
-			.requestMatchers("")
+			.requestMatchers("/api/v1/auth/**")
 			.permitAll()
 			.anyRequest()
 			.authenticated()
